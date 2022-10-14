3f7e073e9ff34c003e5f09f44315c71c888159a3
juliendalbin <julien.dalbin@gmail.com>
2022-10-14 15:16:33 +0200

    ajout de eureka server

---
+++ eureka-server/src/main/java/com/micrommerce/eurekaserver/configurations/ApplicationPropertiesConfiguration.java
@@ -0,0 +1,13 @@
+package com.micrommerce.eurekaserver.configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@ConfigurationProperties("eureka-server")
+@RefreshScope
+public class ApplicationPropertiesConfiguration 
+{
+}
