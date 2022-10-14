84d3a7f5598e3d1b7dae5ee3c7161494e14ba6c9
juliendalbin <julien.dalbin@gmail.com>
2022-10-14 15:39:22 +0200

    configuration de Eureka pour les microservices

--- microservice-commandes/src/main/java/com/mcommandes/configurations/ApplicationPropertiesConfiguration.java
+++ microservice-commandes/src/main/java/com/mcommandes/configurations/ApplicationPropertiesConfiguration.java
@@ -2,7 +2,6 @@ package com.mcommandes.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 
