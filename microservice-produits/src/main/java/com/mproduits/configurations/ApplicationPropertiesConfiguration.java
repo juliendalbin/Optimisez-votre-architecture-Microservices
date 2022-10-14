1fdd8ba10107ce98dab34f2be7b5708855cc517a
juliendalbin <julien.dalbin@gmail.com>
2022-10-14 15:56:44 +0200

    ...

--- microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -2,7 +2,6 @@ package com.mproduits.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 
